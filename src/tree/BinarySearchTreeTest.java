bd342acdbf171a9874f8fb0ddf0c0fe0977b7d35
mayankpanke <panke.mayank@gmail.com>
2019-08-13 20:06:53 +0530

    BST Test

---
+++ src/tree/BinarySearchTreeTest.java
@@ -0,0 +1,48 @@
+package tree;
+
+
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsNull;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class BinarySearchTreeTest {
+
+    @Test
+    public void validateRootEmpty() {
+        BinarySearchTree tree = new BinarySearchTree();
+        Assert.assertThat(tree.getRoot(), IsNull.nullValue());
+    }
+
+    @Test
+    public void validateRootGetsSet() {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.add(10);
+        Assert.assertThat(tree.getRoot().getData(), Is.is(10));
+    }
+
+    @Test
+    public void validateLowerValueSetsOnLeft() {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.add(10);
+        tree.add(5);
+        Assert.assertThat(tree.getRoot().getLeft().getData(), Is.is(5));
+    }
+
+    @Test
+    public void  validateHigherVAlieSetsOnRight() {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.add(10);
+        tree.add(20);
+        Assert.assertThat(tree.getRoot().getRight().getData(), Is.is(20));
+    }
+
+    @Test
+    public void validateLowerToRootButHigherToSecondNodeAddedCorrectly() {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.add(10);
+        tree.add(5);
+        tree.add(9);
+        Assert.assertThat(tree.getRoot().getLeft().getRight().getData(), Is.is(9));
+    }
+}
